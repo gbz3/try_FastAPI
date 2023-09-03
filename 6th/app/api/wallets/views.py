14c4171bf177ccee30ff48763e099d27cadf7a69
gbz3 <1808483+gbz3@users.noreply.github.com>
2023-09-03 10:11:19 +0900

    logging

--- 6th/app/api/wallets/views.py
+++ 6th/app/api/wallets/views.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
+from app.routes import LoggingRoute
 
-router = APIRouter(prefix="/v1/wallets")
+router = APIRouter(prefix="/v1/wallets", route_class=LoggingRoute)
 
 @router.get("")
 async def get_wallets():
